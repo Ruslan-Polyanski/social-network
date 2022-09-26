43b5947c33f4059ea4e6514b9c1ad66b037935c0
Ruslan Polyanski <ruslapolyanski@yandex.by>
2022-09-26 18:44:23 +0300

    init: start social network task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss"
+import 'normalize.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app">
+      <header className="header">
+        <a href="/" className="header__logo">
+          <img src="https://cdn-icons-png.flaticon.com/512/4260/4260120.png" alt="logo" className="header__img"/>
         </a>
       </header>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item"><a href="/" className="nav__link">Profile</a></li>
+          <li className="nav__item"><a href="/" className="nav__link">Messages</a></li>
+          <li className="nav__item"><a href="/" className="nav__link">News</a></li>
+          <li className="nav__item"><a href="/" className="nav__link">Music</a></li>
+        </ul>
+      </nav>
+      <section className="section">
+        <article className="article">
+          <div>
+            <div className="avatar">avatar</div>
+            <div className="description">description</div>
+          </div>
+          <div>
+            <div className="title">My post</div>
+            <div className="post">sdfas asdfasf asdfsaf asddfasdfas asddfasdfas asdfasdfsaf asdfsaf</div>
+          </div>
+        </article>
+ddd
+      </section>
+      <footer className="footer">
+ddd
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
