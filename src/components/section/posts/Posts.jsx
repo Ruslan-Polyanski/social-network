081ebad17218b0872d1b6196fe17c2955b077eb2
Ruslan Polyanski <ruslapolyanski@yandex.by>
2022-09-28 18:50:05 +0300

    feat: add props post

--- src/components/section/posts/Posts.jsx
+++ src/components/section/posts/Posts.jsx
@@ -5,12 +5,12 @@ const Posts = () => {
     return (
       <div>
         <div className="title">My posts</div>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+        <Post text="Text 1" like="8"/>
+        <Post text="Text 2" like="12"/>
+        <Post text="Text 3" like="4"/>
+        <Post text="Text 4" like="7"/>
+        <Post text="Text 5" like="23"/>
+        <Post text="Text 6" like="11"/>
       </div>
     )
 }
--- src/components/section/posts/post/Post.jsx
+++ src/components/section/posts/post/Post.jsx
@@ -1,12 +1,16 @@
 import "./Post.scss";
 
-const Post = () => {
+const Post = (props) => {
     return (
       <div>
         <div>
         <img src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg" alt="avatar" className="post__img"/>
         </div>
-        <div className="post">sdfas asdfasf asdfsaf asddfasdfas asddfasdfas asdfasdfsaf asdfsaf</div>
+        <div className="post">
+          {props.text}
+          <br/>
+          <span>{props.like}</span>
+        </div>
       </div>
     )
 }
