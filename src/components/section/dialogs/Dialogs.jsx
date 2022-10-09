c22603f48ad9e2812bc788263e5caa55952348fd
Ruslan Polyanski <ruslapolyanski@yandex.by>
2022-10-09 07:22:48 +0300

    feat: add textarea in dialogs

--- src/components/section/dialogs/Dialogs.jsx
+++ src/components/section/dialogs/Dialogs.jsx
@@ -1,10 +1,18 @@
+import React from "react";
 import style from "./Dialogs.module.css";
 import DialogItem from "./dilagItem/DialogItem";
 import DialogText from "./dialogText/DialogText";
 
 
 
+
 const Dialogs = ({dataDialogs, dataText}) => {
+
+    const content = React.createRef();
+    
+    const addContent = () => {
+        alert(content.current.value)
+    }
 
     return (
         <div className={style.dialogs}>
@@ -17,6 +25,10 @@ const Dialogs = ({dataDialogs, dataText}) => {
                 {dataText.map((item) => {
                     return <DialogText key={item.id} id={item.id} text={item.text}/>
                 })}
+                <div className="addMessages">
+                    <textarea ref={content}></textarea>
+                    <button onClick={addContent}>Add messages</button>
+                </div>
            </div>
         </div>
     )
