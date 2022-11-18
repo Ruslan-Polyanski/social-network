392f149c533171428d3c8c51776500c76bceac64
Ruslan Polyanski <ruslapolyanski@yandex.by>
2022-11-18 23:00:47 +0300

    feat: add anothe form in loginPage

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,30 +1,35 @@
-import { Formik } from "formik";
+import { Formik, Form, Field } from "formik";
 // import * as yap from "yap";
+
+const FormLogin = (props) => {
+
+    const submitForm = (values) => {
+        console.log(values)
+    }
+
+    return (
+        <div>
+            <Formik initialValues={{ userName: '', userPassword: '' , isSave: false}} onSubmit={submitForm}>
+                {({isSubmitting}) => (                    
+                    <Form>
+                        <Field type="text" name="userName" placeholder="name" /><br/>
+                        <Field type="password" name="userPassword" placeholder="password" /><br/>
+                        <Field type="checkBox" name="isSave" /> remember me<br/>
+                        <button type="submit" disabled={isSubmitting}>Submit</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    )
+}
 
 const Login = (props) => {
     return (
         <>
-        <h1>Login</h1>
-        <div>
-            <Formik
-                initialValues = {{
-                    name: 'No name',
-                    password: 'No password',
-                    remember: true,
-                }}
-                onSubmit={(values) => {console.log(values)}}
-            >
-                {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting}) => (
-                        <form onSubmit={handleSubmit}>
-                            <input type={"name"} name={"name"} onChange={handleChange} onBlur={handleBlur} placeholder="name" value={values.name}/><br/>
-                            <input type={"password"} name={"password"} onChange={handleChange} onBlur={handleBlur} placeholder="password" value={values.password}/><br/>
-                            <input type={"checkbox"} name={"remember"} /> Remember me <br/>
-                            <button type="submit" disabled={isSubmitting}>Submit</button>
-                        </form>
-                    )}
-            </Formik>
-        </div>
+          <h1>Login</h1>
+          <FormLogin/>
         </>
+            
     )
 }
 
